8cb87e73b11c5485defe95d6c89827d2da7962ac
Hai129148 <kagami129148@gmail.com>
2024-02-28 07:51:09 +0700

    cập nhật CourseGui

--- QuanLyKhoaHoc/QuanLyKhoaHoc/src/main/java/GUI/CourseGUI.java
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/src/main/java/GUI/CourseGUI.java
@@ -83,7 +83,7 @@ public class CourseGUI extends javax.swing.JInternalFrame {
         jButtonUpdate = new javax.swing.JButton();
         jButtonDelete = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
@@ -200,6 +200,7 @@ public class CourseGUI extends javax.swing.JInternalFrame {
                                 String departmentId= Integer.toString(x.getDepartmentId());
                                 if(item.equals(departmentId)){
                                     courseInsert_Edit.getjComboBox2().setSelectedIndex(i);
+                                    courseInsert_Edit.getjComboBox2().setEnabled(false);
                                     break;
                                 }
                             }
@@ -243,6 +244,7 @@ public class CourseGUI extends javax.swing.JInternalFrame {
                                 String departmentId= Integer.toString(x.getDepartmentId());
                                 if(item.equals(departmentId)){
                                     courseInsert_Edit.getjComboBox2().setSelectedIndex(i);
+                                    courseInsert_Edit.getjComboBox2().setEnabled(false);
                                     break;
                                 }
                             }
@@ -281,31 +283,7 @@ public class CourseGUI extends javax.swing.JInternalFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Course.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Course.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Course.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Course.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+    public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new CourseGUI().setVisible(true);
