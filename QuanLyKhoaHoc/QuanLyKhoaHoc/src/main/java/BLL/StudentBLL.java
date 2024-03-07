867d22f9cd17c9d7388f5c9b2b2be54808875893
Tran Khoi Nguyen <khoinguyen15023@gmail.com>
2024-03-07 22:22:25 +0700

    Revert "update StudentBLL"
    This reverts commit 40cad085acc957399c46b5bfde3846776d911fc5.

--- QuanLyKhoaHoc/QuanLyKhoaHoc/src/main/java/BLL/StudentBLL.java
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/src/main/java/BLL/StudentBLL.java
@@ -8,41 +8,42 @@ import DAL.StudentDAL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author Acer
  */
 public class StudentBLL {
-
     StudentDAL dal;
-
+    
     public StudentBLL() {
         dal = new StudentDAL();
     }
-
-    public void readStudents() throws SQLException {
-        dal.readStudents();
+    
+    public ArrayList<Student> readStudents() throws SQLException {
+        return dal.readStudents();
     }
-
     public int updateStudent(Student s) throws SQLException {
         return dal.updateStudent(s);
     }
-
     public int insertStudent(Student s) throws SQLException {
         return dal.insertStudent(s);
     }
-
-    public void findStudent(String fullName) throws SQLException {
-        dal.findStudent(fullName);
+    public ArrayList<Student> searchStudents(String value) throws SQLException {
+        ArrayList<Student> list = new ArrayList<>();
+        for (Student x : readStudents()) {
+            String id = Integer.toString(x.getPersonId()).toLowerCase();
+            String firtName = x.getFirstName().toLowerCase();
+            String lastName = x.getLastName().toLowerCase();
+            String hireDate = x.getEnrollmentDate().toLowerCase();
+            if (id.contains(value.toLowerCase()) || firtName.contains(value.toLowerCase()) || lastName.contains(value.toLowerCase()) || hireDate.contains(value.toLowerCase())) {
+                list.add(x);
+            }
+        }
+        return list;
     }
-
     public int deleteStudent(int personID) throws SQLException {
         return dal.deleteStudent(personID);
-    }
-
-    public List<String> getStudentIDs() {
-        return dal.getStudentIDs();
     }
 }
