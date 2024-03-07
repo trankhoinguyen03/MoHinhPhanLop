40cad085acc957399c46b5bfde3846776d911fc5
VanTienKS <130162937+VanTienKS@users.noreply.github.com>
2024-03-07 21:54:58 +0700

    update StudentBLL

--- QuanLyKhoaHoc/QuanLyKhoaHoc/src/main/java/BLL/StudentBLL.java
+++ QuanLyKhoaHoc/QuanLyKhoaHoc/src/main/java/BLL/StudentBLL.java
@@ -8,42 +8,41 @@ import DAL.StudentDAL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Acer
  */
 public class StudentBLL {
+
     StudentDAL dal;
-    
+
     public StudentBLL() {
         dal = new StudentDAL();
     }
-    
-    public ArrayList<Student> readStudents() throws SQLException {
-        return dal.readStudents();
+
+    public void readStudents() throws SQLException {
+        dal.readStudents();
     }
+
     public int updateStudent(Student s) throws SQLException {
         return dal.updateStudent(s);
     }
+
     public int insertStudent(Student s) throws SQLException {
         return dal.insertStudent(s);
     }
-    public ArrayList<Student> searchStudents(String value) throws SQLException {
-        ArrayList<Student> list = new ArrayList<>();
-        for (Student x : readStudents()) {
-            String id = Integer.toString(x.getPersonId()).toLowerCase();
-            String firtName = x.getFirstName().toLowerCase();
-            String lastName = x.getLastName().toLowerCase();
-            String hireDate = x.getEnrollmentDate().toLowerCase();
-            if (id.contains(value.toLowerCase()) || firtName.contains(value.toLowerCase()) || lastName.contains(value.toLowerCase()) || hireDate.contains(value.toLowerCase())) {
-                list.add(x);
-            }
-        }
-        return list;
+
+    public void findStudent(String fullName) throws SQLException {
+        dal.findStudent(fullName);
     }
+
     public int deleteStudent(int personID) throws SQLException {
         return dal.deleteStudent(personID);
+    }
+
+    public List<String> getStudentIDs() {
+        return dal.getStudentIDs();
     }
 }
